c1dc3d6154818aba6115bcdbab616b426c0e7bc7
couvq <quentincouvelaire@gmail.com>
2024-07-20 17:14:39 -0500

    adding new pages/learning about Next.js routing

---
+++ app/posts/[postId]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostById = () => {
+  return (
+    <div>postId is {postId}</div>
+  )
+}
+
+export default PostById
---
+++ app/posts/error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const error = () => {
+  return (
+    <div>An error occurred while loading posts</div>
+  )
+}
+
+export default error
